a01576e8c975a3225d09dcc4765b945cefece360
Marco Carraro <carra.rules@gmail.com>
2023-03-01 17:39:00 +0100

    Check origin

--- src/index.js
+++ src/index.js
@@ -21,14 +21,12 @@ import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
 
-// const x = 0;
-
 /**
  * Every block starts by registering a new block type definition.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType( metadata.name, {
+registerBlockType(metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
@@ -38,4 +36,4 @@ registerBlockType( metadata.name, {
 	 * @see ./save.js
 	 */
 	save,
-} );
+});
